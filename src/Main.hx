d393e149dfce8d195993a8012e6bc40e0d2ace40
DDRKirby(ISQ) <ddrkirby@gmail.com>
2015-08-23 22:30:45 -0700

    v1.06 - fix offbeat note typo

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,7 @@ import com.haxepunk.utils.Key;
 
 class Main extends Engine
 {
-	public static inline var kVersion:String = "v1.05";
+	public static inline var kVersion:String = "v1.06";
 
 	override public function init()
 	{
